d389e4fb089558fac823665d27cc0c9da2f3b90f
Christopher Reeder <sparda-home@Christophers-MacBook-Pro.local>
2022-02-18 14:03:23 -0800

    Updates

---
+++ precache-manifest.0eff81e2a90edca7b3a5e581f65638bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2617473b04cb8430694e936289cdd902",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "3f3035843388be95d3f6",
+    "url": "/my-app/static/css/main.f7030f92.chunk.css"
+  },
+  {
+    "revision": "666a0da8045874495a41",
+    "url": "/my-app/static/js/2.8ec45883.chunk.js"
+  },
+  {
+    "revision": "3f3035843388be95d3f6",
+    "url": "/my-app/static/js/main.a25ad8b5.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
